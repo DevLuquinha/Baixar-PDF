596e4ed3fe5bb1c221745778e457886459ee315e
Outro Lucas <lucasemmanuelrodrigues737@gmail.com>
2025-01-22 13:26:22 -0300

    Correção de Erro Caminho Servidor

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def update_field_sinc(documentID):
 def download_pdf(dictData):
     try:
         path_arch   = "C:/Users/Micro/Desktop/ENVIAR_PDF_SERVIDOR"
-        path_server = "Z:\03-DRIVE CLIENTES\MOSAIC\02-CMA\RVC"
+        path_server = "Z:/03-DRIVE CLIENTES/MOSAIC/02-CMA/RVC"
 
         list_url  = dictData['list_url']
         list_num_proposta = dictData['list_num_proposta']
